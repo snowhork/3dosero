f8da92cda81c3ea101adff2c0bb1406b9efa7ee3
まるまる <romio_cinderella@yahoo.co.jp>
2016-11-03 19:16:20 +0900

    Dive

---
+++ Assets/Dive/Scripts/StartCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartCamera : MonoBehaviour {
+
+  Quaternion gyro;
+	// Use this for initialization
+  void Start () {
+      gyro = Input.gyro.attitude;
+      this.transform.localRotation = Quaternion.Euler(0, -gyro.y, 0);
+  }
+  // Update is called once per frame
+  void LateUpdate () {
+      Input.gyro.enabled = true;
+      if (Input.gyro.enabled)
+      {
+          gyro = Input.gyro.attitude;
+          this.transform.localRotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x,-gyro.y, gyro.z, gyro.w));
+      }
+  }
+
+}
